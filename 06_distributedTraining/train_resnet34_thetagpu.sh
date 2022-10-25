0fa3960c40ac37194241adb34e63537ced407edf
Huihuo Zheng <huihuo.zheng@anl.gov>
2022-10-25 14:21:07 -0500

    Update train_resnet34_thetagpu.sh

--- 06_distributedTraining/train_resnet34_thetagpu.sh
+++ 06_distributedTraining/train_resnet34_thetagpu.sh
@@ -1,9 +1,8 @@
 #!/bin/bash -l
-#COBALT -t 60
-#COBALT -q full-node
+#COBALT -t 20
+#COBALT -q training-gpu
 #COBALT -n 1
-#COBALT -A datascience
-##COBALT -A ALCFAITP
+#COBALT -A ALCFAITP
 
 #  #########3#COBALT --attrs filesystems=home:eagle
 
@@ -14,4 +13,7 @@ conda activate
 # You have to point this to YOUR local copy of ai-science-training-series
 
 export TF_XLA_FLAGS="--tf_xla_auto_jit=2"
-mpirun -np 8 python train_resnet34_hvd.py --num_steps 1000000
+mpirun -np 1 python train_resnet34_hvd.py --num_steps 10
+mpirun -np 2 python train_resnet34_hvd.py --num_steps 10
+mpirun -np 4 python train_resnet34_hvd.py --num_steps 10
+mpirun -np 8 python train_resnet34_hvd.py --num_steps 10
