371c7ace3e022d2354900f634a8222359d1d08cf
vksastry <vsastry@anl.gov>
2024-03-19 03:26:08 +0000

    update bert scripts

--- 07_AITestbeds/Sambanova/bert/BertLarge.sh
+++ 07_AITestbeds/Sambanova/bert/BertLarge.sh
@@ -26,6 +26,8 @@ DIRECTORY=$$
 OUTDIR=${HOME}/${MODEL_NAME}
 
 source ${ACTIVATE}
+PROJ_DIR=${PWD}
+
 cd ${HOME}
 echo "Model: " ${MODEL_NAME} > ${OUTPUT_PATH} 2>&1
 echo "Date: " $(date +%m/%d/%y) >> ${OUTPUT_PATH} 2>&1
@@ -54,7 +56,8 @@ fi
 #######################
 echo "RUN" >> ${OUTPUT_PATH} 2>&1
 env >> ${OUTPUT_PATH} 2>&1
-/usr/local/bin/sbatch --output=${HOME}/slurm-%A.out --ntasks 16 --gres=rdu:8 --ntasks-per-node 16  --nodes 1 --nodelist $(hostname) --cpus-per-task=8  ~/BertLarge_run.sh $1 >> ${OUTPUT_PATH} 2>&1
+echo $PWD
+/usr/local/bin/sbatch --output=${HOME}/slurm-%A.out --ntasks 16 --gres=rdu:8 --ntasks-per-node 16  --nodes 1 --nodelist $(hostname) --cpus-per-task=8  ${PROJ_DIR}/BertLarge_run.sh $1 >> ${OUTPUT_PATH} 2>&1
 
 #######################
 echo "Machine state After: " >> ${OUTPUT_PATH} 2>&1
