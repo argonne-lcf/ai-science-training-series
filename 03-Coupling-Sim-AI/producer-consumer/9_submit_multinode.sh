1db45e947066945c7c8b130d40647ce7baaf76c4
balin <riccardo.balin@colorado.edu>
2025-11-03 15:59:11 +0000

    Fix conda env paths in submit scripts for lecture 3 material

--- 03-Coupling-Sim-AI/producer-consumer/9_submit_multinode.sh
+++ 03-Coupling-Sim-AI/producer-consumer/9_submit_multinode.sh
@@ -13,7 +13,7 @@ cd $PBS_O_WORKDIR
 
 module use /soft/modulefiles
 module load conda/2025-09-25
-conda activate /eagle/datascience/balin/AI4S/_ai4s_simAI
+conda activate /eagle/ALCFAITP/03-Coupling-Sim-AI/_ai4s_simAI
 export TMPDIR=/tmp
 export PATH=$PATH:/opt/pbs/bin
 
